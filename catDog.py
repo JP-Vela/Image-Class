7f1ad6fe4cf3a2b6b1ab2687bcc9ea8c63b29f59
JP-Vela <jangofett122003@gmail.com>
2019-05-02 13:25:54 -0600

    Change more stuff

--- catDog.py
+++ catDog.py
@@ -33,22 +33,22 @@ model =Sequential()
 
 model.add(Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(MaxPooling2D(pool_size=(4,4)))
 
 model.add(Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(MaxPooling2D(pool_size=(4,4)))
 
 model.add(Conv2D(64,(3,3), input_shape=(img_width, img_height, 3)))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(MaxPooling2D(pool_size=(4,4)))
 
 model.add(Flatten())
 model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
-model.add(Activation('sigmoid'))
+model.add(Activation('softmax'))
 
 model.compile(loss='binary_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
 
