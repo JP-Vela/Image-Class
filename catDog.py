1c0b398287bea967aef864c6b88b0b7f05fa8cc1
JP-Vela <jangofett122003@gmail.com>
2019-05-02 18:17:42 -0600

    Added more conv layers and changed some activation funtion stuff.

--- catDog.py
+++ catDog.py
@@ -3,7 +3,7 @@ from keras.layers import Dense, Activation, Dropout, Flatten
 from keras import optimizers
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator
-import numpy as np 
+import numpy as np
 
 # step 1: load data
 
@@ -18,7 +18,7 @@ train_generator = datagen.flow_from_directory(directory=train_data_dir,
 											   target_size=(img_width,img_height),
 											   classes=['dogs','cats'],
 											   class_mode='binary',
-											   batch_size=4)
+											   batch_size=16)
 
 validation_generator = datagen.flow_from_directory(directory=valid_data_dir,
 											   target_size=(img_width,img_height),
@@ -29,26 +29,47 @@ validation_generator = datagen.flow_from_directory(directory=valid_data_dir,
 
 # step-2 : build model
 
-model =Sequential()
+model =Sequential([
+	Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)),
+	Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)),
+	Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)),
+	#Activation('sigmoid'),
+	MaxPooling2D(pool_size=(2,2)),
+	Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)),
+	Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)),
+	Activation('sigmoid'),
+	MaxPooling2D(pool_size=(2,2)),
+	Conv2D(64,(3,3), input_shape=(img_width, img_height, 3)),
+	MaxPooling2D(pool_size=(2,2)),
+	Flatten(),
+	Dense(64, activation='sigmoid'),
+	Dense(1, activation='softmax')
+	#Activation('softmax')
 
-model.add(Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(4,4)))
+])
 
-model.add(Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(4,4)))
+#model.add(Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)))
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(4,4)))
 
-model.add(Conv2D(64,(3,3), input_shape=(img_width, img_height, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(4,4)))
+#model.add(Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)))
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(4,4)))
 
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('softmax'))
+#model.add(Conv2D(42,(3,3), input_shape=(img_width, img_height, 3)))
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(4,4)))
+
+#model.add(Conv2D(64,(3,3), input_shape=(img_width, img_height, 3)))
+#model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(4,4)))
+
+#model.add(Flatten())
+#model.add(Dense(64))
+#model.add(Activation('relu'))
+#model.add(Dropout(0.5))
+#model.add(Dense(1))
+#model.add(Activation('softmax'))
 
 model.compile(loss='binary_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
 
