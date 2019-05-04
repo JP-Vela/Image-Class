84b5032ce73b13c44aa41008d5703e4ffafff2ef
JP-Vela <jangofett122003@gmail.com>
2019-05-04 00:11:49 -0600

    Another update

--- catDog.py
+++ catDog.py
@@ -33,52 +33,31 @@ model =Sequential([
 	Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)),
 	Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)),
 	Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)),
-	#Activation('sigmoid'),
 	MaxPooling2D(pool_size=(2,2)),
-	Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)),
-	Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)),
-	Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)),
-	#Activation('relu'),
+	Conv2D(42,(3,3), input_shape=(img_width, img_height, 3)),
+	Conv2D(42,(3,3), input_shape=(img_width, img_height, 3)),
+	Conv2D(42,(3,3), input_shape=(img_width, img_height, 3)),
 	MaxPooling2D(pool_size=(2,2)),
-	Conv2D(64,(3,3), input_shape=(img_width, img_height, 3)),
+	Conv2D(42,(3,3), input_shape=(img_width, img_height, 3)),
+	Conv2D(52,(3,3), input_shape=(img_width, img_height, 3)),
+	Conv2D(52,(3,3), input_shape=(img_width, img_height, 3)),
+	Activation('relu'),
+	MaxPooling2D(pool_size=(2,2)),
+	Conv2D(54,(3,3), input_shape=(img_width, img_height, 3)),
 	MaxPooling2D(pool_size=(2,2)),
 	Flatten(),
-	Dense(62, activation='relu'),
+	Dense(64, activation='relu'),
 	Dense(64, activation='relu'),
 	Dense(1, activation='softmax')
-	#Activation('softmax')
 
 ])
-
-#model.add(Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)))
-#model.add(Activation('relu'))
-#model.add(MaxPooling2D(pool_size=(4,4)))
-
-#model.add(Conv2D(32,(3,3), input_shape=(img_width, img_height, 3)))
-#model.add(Activation('relu'))
-#model.add(MaxPooling2D(pool_size=(4,4)))
-
-#model.add(Conv2D(42,(3,3), input_shape=(img_width, img_height, 3)))
-#model.add(Activation('relu'))
-#model.add(MaxPooling2D(pool_size=(4,4)))
-
-#model.add(Conv2D(64,(3,3), input_shape=(img_width, img_height, 3)))
-#model.add(Activation('relu'))
-#model.add(MaxPooling2D(pool_size=(4,4)))
-
-#model.add(Flatten())
-#model.add(Dense(64))
-#model.add(Activation('relu'))
-#model.add(Dropout(0.5))
-#model.add(Dense(1))
-#model.add(Activation('softmax'))
 
 model.compile(optimizers.Adam(lr=0.01),loss='binary_crossentropy',metrics=['accuracy'])
 
 print('model complied!!')
 
 print('starting training....')
-training = model.fit_generator(generator=train_generator, steps_per_epoch=2048 // 16,epochs=40,validation_data=validation_generator,validation_steps=832//16)
+training = model.fit_generator(generator=train_generator, steps_per_epoch=2048 // 16,epochs=40,validation_data=validation_generator,validation_steps=832//32)
 
 print('training finished!!')
 
