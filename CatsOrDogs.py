81d79677c2ab6444896093d9faa98fafb94a2295
JP-Vela <jangofett122003@gmail.com>
2019-05-05 13:51:24 -0600

    Working now

---
+++ CatsOrDogs.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers import Dense, Activation, Dropout, Flatten
+from keras import optimizers
+from keras.models import Sequential
+from keras.metrics import categorical_crossentropy
+
+
+DATADIR = "/Users/johnpaul/Documents/Programming/MachineLearning/Image-Class2/data/train/"
+CATEGORIES = ["dogs","cats"]
+
+for category in CATEGORIES:
+    path = os.path.join(DATADIR, category)
+    for img in os.listdir(path):
+        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+        plt.imshow(img_array, cmap="gray")
+        plt.show()
+        break
+    break
+
+
+# In[2]:
+
+
+print(img_array)
+
+
+# In[3]:
+
+
+IMG_SIZE = 50
+
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap="gray")
+plt.show()
+
+
+# In[4]:
+
+
+training_data = []
+
+def createTrainingData():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category)
+        class_num = CATEGORIES.index(category)
+        for img in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([new_array,class_num])
+            except Exception as e:
+                pass
+
+createTrainingData()
+
+
+# In[5]:
+
+
+print(len(training_data))
+
+
+# In[6]:
+
+
+import random
+random.shuffle(training_data)
+
+
+# In[7]:
+
+
+for sample in training_data[:10]:
+    print(sample[1])
+
+
+# In[8]:
+
+
+X = []
+y = []
+
+
+# In[9]:
+
+
+for features, label in training_data:
+    X.append(features)
+    y.append(label)
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+
+# In[10]:
+
+
+X=X/255
+training_batch_size=16;
+
+
+# In[16]:
+
+
+model = Sequential()
+model.add(Conv2D(62, (3,3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+             
+model.add(Conv2D(62, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+             
+model.add(Conv2D(62, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+             
+             
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+
+# In[17]:
+
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+
+# In[18]:
+
+
+model.fit(X,y, validation_split=0.1, epochs=10)
+
+
+# In[ ]:
+
+
+
+
