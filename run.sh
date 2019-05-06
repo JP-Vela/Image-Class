b1873128eb5034bdd8b942f31c574d23e3867856
JP-Vela <jangofett122003@gmail.com>
2019-05-05 22:25:36 -0700

    Changed run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-git add CatsOrDogs.py
-git commit -m "Another update to the model structure for training"
-git push
+#git add CatsOrDogs.py
+#git commit -m "Another update to the model structure for training"
+#git push
 
 spell run python3 CatsOrDogs.py
 
