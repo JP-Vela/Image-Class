8cdd42334062bd9680a5daacd743f23cccf2e9c8
JP-Vela <jangofett122003@gmail.com>
2019-05-05 21:15:57 -0700

    More changes to structure of model

--- CatsOrDogs.py
+++ CatsOrDogs.py
@@ -112,14 +112,25 @@ training_batch_size=16;
 
 
 model = Sequential()
-model.add(Conv2D(128, (3,3), input_shape=X.shape[1:]))
-model.add(Conv2D(128, (3,3), input_shape=X.shape[1:]))
-model.add(Conv2D(128, (3,3), input_shape=X.shape[1:]))
+model.add(Conv2D(62, (3,3), input_shape=X.shape[1:]))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
+model.add(Conv2D(62, (3,3)))
+model.add(Conv2D(62, (3,3)))
+model.add(Conv2D(62, (3,3)))
+model.add(Conv2D(62, (3,3)))
+
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(62, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+
 model.add(Flatten())
-model.add(Dense(128))
+model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
